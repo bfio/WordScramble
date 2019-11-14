7b7a3764f3cdb6751755a81ac48fe01a514b96d3
Brendan Fiorani <fbrendan9@gmail.com>
2019-11-13 23:26:15 -0500

    Created Difficulty enum

---
+++ src/scramble/Difficulty.java
@@ -0,0 +1,21 @@
+package scramble;
+
+public enum Difficulty {
+	EASY("Easy", "Good for new players with less experience"), 
+	MEDIUM("Medium", "Better for intermediate players with some experience"), 
+	HARD("Hard", "Hardest difficulty suited for the most experienced players");
+	
+	private String name;
+	private String description;
+	
+	Difficulty(String name, String description) {
+		this.name = name;
+		this.description = description;
+	}
+	
+	@Override
+	public String toString() {
+		return description;
+	}
+	
+}
