4f7dccbbdba593e9d6ccfa3d25b0637157e0abdc
Niels Lashbrook <nielsdog@gmail.com>
2019-12-01 12:40:22 -0500

    Created the word scrambling algorithm

--- src/scramble/Scrambler.java
+++ src/scramble/Scrambler.java
@@ -1,11 +1,22 @@
 package scramble;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public interface Scrambler {
 	
 	public static String scrambleWord(String toScramble) {
 
-		char [] temp = toScramble.toCharArray();
-		return toScramble + " but scrambled";
+		String[] scrambleArray = toScramble.split("");
+        List<String> letters = Arrays.asList(scrambleArray);
+        Collections.shuffle(letters);
+        StringBuilder sb = new StringBuilder(toScramble.length());
+        for (String c : letters) {
+            sb.append(c);
+        }
+        return sb.toString() + " but scrambled";
+		
 	}
 
 	public static int calculateValue(String phrase) {
